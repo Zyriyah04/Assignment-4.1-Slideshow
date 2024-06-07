53dd3660ec26cb88d4a45442c5ad6dea767cc674
Zyriyah04 <ZYRSTOKE@uat.edu>
2024-06-07 01:11:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slideshow-image');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide(currentIndex);
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+showSlide(currentIndex);
